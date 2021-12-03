0656a84d9960a3cad7af684d211e5e1cf4c71f62
BratPodz <94909565+BratPodz@users.noreply.github.com>
2021-12-03 22:29:13 +0500

    Add files via upload

---
+++ IS-2-19-UmurzakovRZ/Form2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace IS_2_19_UmurzakovRZ
+{
+    public partial class Form2 : Form
+    {
+        class Connect
+        {
+            public static string Co()
+            {
+                string Host = "caseum.ru";
+                int Port = 33333;
+                string User = "test_user";
+                string Db = "db_test";
+                string Pass = "test_pass";
+                string connStr = $"server={Host};" +
+                $"port={Port};" +
+                $"user={User};" +
+                $"database={Db};" +
+                $"password={Pass};";
+                return connStr;
+            }
+        }
+        public Form2()
+        {
+           InitializeComponent();
+        }
+        MySqlConnection conn = new MySqlConnection(Connect.Co());
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            try
+            {
+                MessageBox.Show("Подключение");
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка подключения");
+            }
+            finally
+            {
+                MessageBox.Show($"Вы успешно подключены!");
+            }
+            conn.Close();
+        }
+    }
+}
---
+++ IS-2-19-UmurzakovRZ/Form5.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary7;
+using MySql.Data.MySqlClient;
+
+namespace IS_2_19_UmurzakovRZ
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        MySqlConnection conn = new MySqlConnection(Con.C());
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+
+        }
+        public bool InsertStudents(string fiostud, string registration_date)
+        {
+            int InsertCount = 0;
+            bool result = false;
+            conn.Open();
+            string query = $"INSERT INTO t_PraktStud (fioStud, datetimeStud) VALUES ('{fiostud}', '{registration_date}')";
+            try
+            {
+                MySqlCommand command = new MySqlCommand(query, conn);
+                InsertCount = command.ExecuteNonQuery();
+            }
+            catch
+            {
+                InsertCount = 0;
+            }
+            finally
+            {
+                conn.Close();
+                if (InsertCount != 0)
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Fio_studenta = textBox1.Text;
+            string Reg_date = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            if (textBox1.Text.Equals(""))
+            {
+                MessageBox.Show("Вы не ввели все необходимые данные!");
+            }
+            else
+            {
+                InsertStudents(Fio_studenta, Reg_date);
+            }
+        }
+    }
+}
