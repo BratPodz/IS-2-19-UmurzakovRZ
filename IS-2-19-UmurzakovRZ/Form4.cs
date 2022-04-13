81c80a6ecdf4de92c0133429f92427b7476df36c
Рустам Умурзаков <victormo1337@hotmail.com>
2022-04-13 14:36:42 +0500

    Четвертая кнопка

--- IS-2-19-UmurzakovRZ/Form4.cs
+++ IS-2-19-UmurzakovRZ/Form4.cs
@@ -33,6 +33,7 @@ namespace IS_2_19_UmurzakovRZ
             catch
             {
                 MessageBox.Show("Ошибка подключения");
+                this.Close;
             }
             finally
             {
